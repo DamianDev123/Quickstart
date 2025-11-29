728eca48f74a483bbb84083e24eb4a6222ca7336
moosecoding <moosecoding@moosecoding.my.domain>
2025-11-29 00:38:29 -0500

    Moving init to onstart

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/TeleOP.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/next/TeleOP.kt
@@ -39,13 +39,13 @@ class TeleOP: NextFTCOpMode() {
     }
 
     override fun onInit() {
-        InitActuators.init.schedule() // Or a LambdaCommand
-        Gamepads.gamepad1.a whenBecomesTrue ShootBalls();
-        Gamepads.gamepad1.rightTrigger.greaterThan(0.3) whenBecomesTrue Intake.runIntake whenBecomesFalse Intake.stopIntake
 
     }
 
     override fun onStartButtonPressed() {
+        InitActuators.init.schedule() // Or a LambdaCommand
+        Gamepads.gamepad1.a whenBecomesTrue ShootBalls();
+        Gamepads.gamepad1.rightTrigger.greaterThan(0.3) whenBecomesTrue Intake.runIntake whenBecomesFalse Intake.stopIntake
     }
 
     override fun onUpdate() {
